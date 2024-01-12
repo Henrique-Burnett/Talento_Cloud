2150764853178c0db1cf87c2d1371c8767a05ec2
Henrique-Burnett <henriquecbm@hotmail.com>
2024-01-11 22:05:17 -0300

    D6 - JS

--- JAVASCRIPT/D6 - JS/script.js
+++ JAVASCRIPT/D6 - JS/script.js
@@ -1,8 +1,19 @@
-titulo = document.createElement('h1');
+let titulo = document.createElement('h1');
 
-titulo.innerHTML = '<h1 id="titulo">Criando elementos no DOM com o JS</h1>'
+titulo.id =  'titulo';
+titulo.innerText = 'Morato Pizzería'
 
-produto = document.createElement('article');
+let body = document.querySelector('body');
 
-produto.innerHTML = ``
+body.appendChild(titulo);
 
+let produto = document.createElement('article');
+
+produto.innerHTML = `  <article>
+<h2>Pizza Corifeu</h2>
+<img src="https://cdn.accon.app/1694692784286946858655001277-1080p.jpg" alt="Pizza de lâminas de abobrinha com queijo parmesão e mussarela">
+<p>Pizza de lâminas de abobrinha com queijo parmesão e mussarela.</p>
+<p id="preco-x-salada">R$ 74.90</p>
+</article>`;
+
+body.appendChild(produto);
